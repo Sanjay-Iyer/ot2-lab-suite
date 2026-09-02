35d73cf5f4cc31a25010117148e80b166e6ba11d
Sanjay-Iyer <sanjayiyer01@gmail.com>
2026-09-02 15:40:01 -0400

    mixed stars n dye

--- scripts/run_printing_experiment_robot.py
+++ scripts/run_printing_experiment_robot.py
@@ -168,6 +168,9 @@ WORKFLOWS = {
     "a11-paper5-column11-height-sweep": (
         "standard",
         "configs/experiments/32_a11_paper5_column11_height_sweep.yaml"),
+    "a11-paper5-column10-height-sweep": (
+        "standard",
+        "configs/experiments/33_a11_paper5_column10_height_sweep.yaml"),
     "add-dye": (
         "dilution", "configs/experiments/09_add_dye_to_a11.yaml"),
     "prepare-c11-d11": (
